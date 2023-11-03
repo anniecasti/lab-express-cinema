e30f6ce3430b7e2c61f6c5a23c7c714aac8d4cfd
Anggie Castillo <anniecastillono@outlook.com>
2023-11-03 11:40:03 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const MOVIE = require('../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res) => {
+    MOVIE
+        .find()
+        .then(movies => res.render('movies', { movies }))
+        .catch(err => console.log(err))
+})
+router.get('/movies/:movies_id', (req, res) => {
+
+    const { _id } = req.params
+
+    MOVIE
+        .findById(req.params._id)
+        .then(movie => res.render('details', { movie }))
+        .catch(err => console.log(err))
+
+})
 module.exports = router;
+
